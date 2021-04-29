277a73a4f3df2946d5d3fab405f134560af29835
Xplt3 <81273481+Xplt3@users.noreply.github.com>
2021-04-29 22:22:09 +0800

    Add files via upload

---
+++ admfinder.php
@@ -0,0 +1,50 @@
+<?php
+print "   
+	
+ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+
+__  __      _ _   _____
+\ \/ /_ __ | | |_|___ /
+ \  /| '_ \| | __| |_ \
+ /  \| |_) | | |_ ___) |
+/_/\_\ .__/|_|\__|____/
+     |_|
+";
+
+echo "masukan site  : ";
+$target = trim(fgets(STDIN));
+$list = "xplt3_wordlist.txt";
+if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
+	$targetnya = "http://$target";
+}else{
+	$targetnya = $target;
+}
+
+$buka = fopen("$list","r");
+$ukuran = filesize("$list");
+$baca = fread($buka,$ukuran);
+$lists = explode("\r\n",$baca);
+
+foreach($lists as $login){
+	$log = "$targetnya/$login";
+	$ch = curl_init("$log");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($httpcode == 200){
+		 $handle = fopen("xplt3-result.txt", "a+");
+		fwrite($handle, "$log\n");
+		print "\n\n [".date('H:m:s')."] Mencoba : $log => Ditemukan\n";
+	}else{
+		print "\n[".date('H:m:s')."] Mencoba : $log => tidak di temukan";
+	}
+}
+  
+?>
